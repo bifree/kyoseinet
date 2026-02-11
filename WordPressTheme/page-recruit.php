94535754d79964b9694d60e684ae81afd1f223b2
bifree <bifree2bfree@gmail.com>
2026-02-11 20:28:54 +0900

    採用情報追加

---
+++ WordPressTheme/page-recruit.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div class="recruit" id="page">
+  <div class="l-inner page__inner">
+    <div class="page__title">
+      <span class="page__title-en page__title-en--ore">Recruit</span>
+      <h2 class="page__title-jp">採用情報</h2>
+    </div>
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
